eba90bdfee94a751ccd77f4b69fab67c8224423e
Dyrago <damianworek@protonmail.com>
2025-11-19 08:14:01 +0100

    Clean

--- DbScripts/domains.sql
+++
@@ -1,8 +0,0 @@
--- domena dla identyfikatora
-CREATE DOMAIN DM_ID AS INTEGER NOT NULL;
-
--- domena dla nazw użytkowników
-CREATE DOMAIN DM_USERNAME AS VARCHAR(50) NOT NULL;
-
--- domena dla daty i czasu
-CREATE DOMAIN DM_DATETIME AS TIMESTAMP;
